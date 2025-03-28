bc1052ed050860acc4b50a97fc71ab07f51fdb02
mahmoudyounis0 <mahmmoudyounis5761@gmail.com>
2025-03-28 02:36:06 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,7 @@ export default {
       screens: {
         mobile: "320px",
         tablet: "768px",
-        // => @media (min-width: 640px) { ... }
-
         laptop: "1025px",
-        // => @media (min-width: 1024px) { ... }
-
         desktop: "1279px",
       },
     },
